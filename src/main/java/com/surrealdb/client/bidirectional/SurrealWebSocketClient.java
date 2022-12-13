4d20f02507cc257748871924c8f0789928d58f75
DamianKocher <damian@dmnkchr.com>
2022-12-13 11:44:06 -0600

    refactor: move internal ws client to its own class

--- src/main/java/com/surrealdb/client/bidirectional/SurrealWebSocketClient.java
+++ src/main/java/com/surrealdb/client/bidirectional/SurrealWebSocketClient.java
@@ -1,26 +1,15 @@
 package com.surrealdb.client.bidirectional;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import com.surrealdb.auth.SurrealAuthCredentials;
 import com.surrealdb.client.SurrealBiDirectionalClient;
-import com.surrealdb.client.bidirectional.rpc.RpcRequest;
-import com.surrealdb.client.bidirectional.rpc.RpcResponse;
-import com.surrealdb.client.communication.CommunicationManager;
-import com.surrealdb.client.communication.RequestEntry;
 import com.surrealdb.client.settings.SurrealClientSettings;
 import com.surrealdb.client.settings.SurrealConnectionProtocol;
-import com.surrealdb.exception.SurrealConnectionTimeoutException;
 import com.surrealdb.exception.SurrealException;
 import com.surrealdb.exception.SurrealExceptionUtils;
-import com.surrealdb.exception.SurrealNotConnectedException;
 import com.surrealdb.query.QueryResult;
 import lombok.experimental.NonFinal;
-import lombok.extern.slf4j.Slf4j;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.exceptions.WebsocketNotConnectedException;
-import org.java_websocket.handshake.ServerHandshake;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
@@ -29,9 +18,6 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 import static com.surrealdb.gson.SurrealGsonUtils.createSurrealCompatibleGsonInstance;
 
@@ -173,113 +159,5 @@ public class SurrealWebSocketClient implements SurrealBiDirectionalClient {
     @Override
     public @NotNull ExecutorService getAsyncOperationExecutorService() {
         return settings.getAsyncOperationExecutorService();
-    }
-
-    @Slf4j
-    private static class InternalWebsocketClient extends WebSocketClient {
-
-        @NotNull Gson gson;
-        @NotNull CommunicationManager communicationManager;
-        @NotNull AtomicLong lastRequestId;
-
-        @NotNull Consumer<Boolean> onClose;
-
-        @NotNull ExecutorService executorService;
-
-        InternalWebsocketClient(@NotNull SurrealClientSettings settings, @NotNull Gson gson, @NotNull Consumer<Boolean> onClose) {
-            super(settings.getUri());
-
-            this.gson = gson;
-            this.communicationManager = new CommunicationManager(gson);
-            this.onClose = onClose;
-
-            this.lastRequestId = new AtomicLong();
-
-            this.executorService = settings.getAsyncOperationExecutorService();
-        }
-
-        public <U> @NotNull CompletableFuture<U> rpc(@NotNull String method, @NotNull Type resultType, Object @NotNull ... arguments) {
-            return CompletableFuture.supplyAsync(() -> {
-                String id = Long.toString(lastRequestId.getAndIncrement());
-                RpcRequest rpcRequest = new RpcRequest(id, method, arguments);
-
-                try {
-                    RequestEntry<U> requestEntry = communicationManager.createRequest(id, method, resultType);
-                    String payloadString = gson.toJson(rpcRequest);
-                    log.debug("Outgoing RPC [data: {}", payloadString);
-                    send(payloadString);
-                    return requestEntry.getCallback();
-                } catch (Exception exception) {
-                    communicationManager.cancelRequest(id, exception);
-
-                    if (exception instanceof WebsocketNotConnectedException) {
-                        throw new SurrealNotConnectedException();
-                    }
-
-                    throw SurrealExceptionUtils.wrapException(exception, "Failed to send RPC request");
-                }
-            }, executorService).thenCompose(Function.identity());
-        }
-
-        @Override
-        public void onMessage(@NotNull String rawMessage) {
-            JsonElement message = gson.fromJson(rawMessage, JsonElement.class);
-            RpcResponse response = gson.fromJson(message, RpcResponse.class);
-
-            log.debug("Incoming RPC [data: {}]", rawMessage);
-
-            response.getError().ifPresentOrElse(
-                error -> communicationManager.completeRequest(response.getId(), error),
-                () -> communicationManager.completeRequest(response.getId(), response.getResult())
-            );
-        }
-
-        @NotNull CompletableFuture<Void> connectAsync(long timeout, @NotNull TimeUnit timeUnit) {
-            return CompletableFuture.runAsync(() -> {
-                if (isOpen()) {
-                    log.debug("Already connected to Surreal server, connect request...");
-                    return;
-                }
-
-                try {
-                    log.debug("Connecting to Surreal server [uri: {}, timeout: {} {}]", getURI(), timeout, timeUnit);
-                    connectBlocking(timeout, timeUnit);
-                } catch (InterruptedException ignored) {
-                }
-
-                if (!isOpen()) {
-                    throw new SurrealConnectionTimeoutException();
-                }
-            }, executorService);
-        }
-
-        public @NotNull CompletableFuture<Void> disconnectAsync() {
-            return CompletableFuture.runAsync(() -> {
-                try {
-                    log.debug("Gracefully disconnecting from Surreal server");
-                    closeBlocking();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }, executorService);
-        }
-
-        @Override
-        public void onOpen(ServerHandshake handshakeData) {
-            log.debug("Connected to Surreal server '{}'", getURI());
-        }
-
-        @Override
-        public void onClose(int code, String reason, boolean remote) {
-            log.debug("Disconnected from Surreal server. [code: {}, reason: {}, caused by remote: {}]", code, reason, remote);
-
-            communicationManager.cancelAllRequests(new SurrealNotConnectedException());
-            onClose.accept(remote);
-        }
-
-        @Override
-        public void onError(Exception exception) {
-            log.error("A WebSocket error occurred", exception);
-        }
     }
 }
