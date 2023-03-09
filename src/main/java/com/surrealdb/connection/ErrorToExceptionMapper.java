6212755a36d9221736e9f357299e940a8cc52c56
coder966 <coder966@gmail.com>
2023-03-10 00:20:07 +0300

    #10 support unique index violation errors

--- src/main/java/com/surrealdb/connection/ErrorToExceptionMapper.java
+++ src/main/java/com/surrealdb/connection/ErrorToExceptionMapper.java
@@ -1,9 +1,6 @@
 package com.surrealdb.connection;
 
-import com.surrealdb.connection.exception.SurrealAuthenticationException;
-import com.surrealdb.connection.exception.SurrealException;
-import com.surrealdb.connection.exception.SurrealNoDatabaseSelectedException;
-import com.surrealdb.connection.exception.SurrealRecordAlreadyExitsException;
+import com.surrealdb.connection.exception.*;
 import com.surrealdb.connection.model.RpcResponse;
 
 import java.util.regex.Matcher;
@@ -14,6 +11,7 @@ import java.util.regex.Pattern;
  */
 public class ErrorToExceptionMapper {
     private final static Pattern RECORD_ALREADY_EXITS_PATTERN = Pattern.compile("There was a problem with the database: Database record `(.+):(.+)` already exists");
+    private final static Pattern UNIQE_INDEX_VIOLATION_PATTERN = Pattern.compile("There was a problem with the database: Database index `(.+)` already contains \\[.+\\], with record `(.+):(.+)`");
 
     public static SurrealException map(RpcResponse.Error error){
         if (error.getMessage().contains("There was a problem with authentication")) {
@@ -27,6 +25,11 @@ public class ErrorToExceptionMapper {
         Matcher recordAlreadyExitsMatcher = RECORD_ALREADY_EXITS_PATTERN.matcher(error.getMessage());
         if (recordAlreadyExitsMatcher.matches()) {
             return new SurrealRecordAlreadyExitsException(recordAlreadyExitsMatcher.group(1), recordAlreadyExitsMatcher.group(2));
+        }
+
+        Matcher uniqueIndexViolationPattern = UNIQE_INDEX_VIOLATION_PATTERN.matcher(error.getMessage());
+        if (uniqueIndexViolationPattern.matches()) {
+            return new UniqueIndexViolationException(uniqueIndexViolationPattern.group(2), uniqueIndexViolationPattern.group(1), uniqueIndexViolationPattern.group(3));
         }
 
         // return the generic Exception
