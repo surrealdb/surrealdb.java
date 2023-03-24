d97a6abaaa486101f60a1016fc1c132831845abb
Przemyslaw Hugh Kaznowski <hughkaznowski@protonmail.com>
2023-03-24 16:19:04 +0000

    Jdbc setup (#24)
    * Create necessary files for JDBC implementation
    
    * Add jdbc stubs unimplemented exception
    
    * Fixup some format stuff

---
+++ src/main/java/com/surrealdb/jdbc/SurrealJDBCDriver.java
@@ -0,0 +1,46 @@
+package com.surrealdb.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class SurrealJDBCDriver implements Driver {
+    @Override
+    public Connection connect(String url, Properties info) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean acceptsURL(String url) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getMajorVersion() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getMinorVersion() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        throw new UnsupportedOperationException();
+    }
+}
