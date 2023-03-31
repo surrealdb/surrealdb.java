4ec935a755cbc6ae678fda98d57ec308f3d88cbf
LOSTEDs <89002511+LOSTEDs@users.noreply.github.com>
2023-03-31 03:01:43 -0400

    Refactor RemovePatch to use Java Record

--- src/main/java/com/surrealdb/driver/model/patch/RemovePatch.java
+++ src/main/java/com/surrealdb/driver/model/patch/RemovePatch.java
@@ -1,17 +1,10 @@
 package com.surrealdb.driver.model.patch;
 
-import lombok.Getter;
-
 /**
  * @author Khalid Alharisi
  */
-@Getter
-public class RemovePatch implements Patch {
-    private final String op = "remove";
-    private final String path;
-
+public record RemovePatch(String op, String path) implements Patch {
     public RemovePatch(String path) {
-        this.path = path;
+        this("remove", path);
     }
-
 }
