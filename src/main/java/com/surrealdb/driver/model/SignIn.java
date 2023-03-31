e195038c97ad39f2f52abfa0d962aa21441ebe4b
LOSTEDs <89002511+LOSTEDs@users.noreply.github.com>
2023-03-31 03:25:56 -0400

    Refactor SignIn to use Java Record

--- src/main/java/com/surrealdb/driver/model/SignIn.java
+++ src/main/java/com/surrealdb/driver/model/SignIn.java
@@ -1,14 +1,7 @@
 package com.surrealdb.driver.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 /**
  * @author Khalid Alharisi
  */
-@Data
-@AllArgsConstructor
-public class SignIn {
-    private String user;
-    private String pass;
+public record SignIn(String user, String pass) {
 }
