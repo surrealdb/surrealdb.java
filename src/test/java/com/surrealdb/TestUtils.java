42f93ae566860204767f99c29e08d44b4f82d672
maxmielchen <max@mielchen.email>
2023-04-07 14:27:21 +0200

    inline environment variables

--- src/test/java/com/surrealdb/TestUtils.java
+++ src/test/java/com/surrealdb/TestUtils.java
@@ -5,14 +5,14 @@ package com.surrealdb;
  */
 public class TestUtils {
 
-    private static final String HOST = System.getenv("TEST_SURREAL_HOST");
-    private static final int PORT = Integer.parseInt(System.getenv("TEST_SURREAL_PORT"));
+    private static final String HOST = "localhost";
+    private static final int PORT = 8000;
 
-    private static final String USERNAME = System.getenv("TEST_SURREAL_USERNAME");
-    private static final String PASSWORD = System.getenv("TEST_SURREAL_PASSWORD");
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
-    private static final String NAMESPACE = System.getenv("TEST_SURREAL_NAMESPACE");
-    private static final String DATABASE = System.getenv("TEST_SURREAL_DATABASE");
+    private static final String NAMESPACE = "test";
+    private static final String DATABASE = "test";
 
     public static String getHost(){
         return HOST;
