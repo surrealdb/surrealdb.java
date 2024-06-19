2fc501b92b2fa804b1602b14b557e6ffe8d88f26
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-06-19 23:11:09 +0100

    Improve loader

--- src/main/java/com/surrealdb/Loader.java
+++ src/main/java/com/surrealdb/Loader.java
@@ -29,28 +29,30 @@ class Loader {
     private static String get_path() {
         final String arch = System.getProperty("os.arch").toLowerCase(Locale.ENGLISH);
         final String name = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
-        if (name.contains("nix") || name.contains("nux")) {
-            if (arch.contains("aarch64")) {
+        final boolean intel = arch.contains("x86_64") || arch.contains("amd64");
+        final boolean arm = arch.contains("aarch64");
+        final boolean linux = name.contains("nix") || name.contains("nux");
+        final boolean windows = name.contains("win");
+        final boolean osx = name.contains("mac");
+        if (linux) {
+            if (arm)
                 return "linux_arm64";
-            } else if (arch.contains("x86_64") || arch.contains("amd64")) {
+            else if (intel)
                 return "linux_64";
-            }
-        } else if (name.contains("win")) {
-            if (arch.contains("x86_64")) {
+        } else if (windows) {
+            if (intel)
                 return "windows_64";
-            }
-        } else if (name.contains("mac")) {
-            if (arch.contains("aarch64")) {
+        } else if (osx) {
+            if (arm)
                 return "osx_arm64";
-            } else if (arch.contains("x86_64")) {
+            else if (intel)
                 return "osx_64";
-            }
         }
         throw new RuntimeException("Unsupported architecture: " + arch + " - name: " + name);
     }
 
     private static void copy(InputStream input, OutputStream output) throws IOException {
-        byte[] buffer = new byte[8192];
+        final byte[] buffer = new byte[8192];
         int bytesRead;
         while ((bytesRead = input.read(buffer)) != -1) {
             output.write(buffer, 0, bytesRead);
