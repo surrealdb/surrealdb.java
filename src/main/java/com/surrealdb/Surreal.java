1164974fba5ef7094b79df60cc211c176462bee7
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-10-16 12:03:08 +0100

    Wip on Javadoc

--- src/main/java/com/surrealdb/Surreal.java
+++ src/main/java/com/surrealdb/Surreal.java
@@ -102,11 +102,28 @@ public class Surreal extends Native implements AutoCloseable {
     @Override
     final native boolean deleteInstance(long ptr);
 
+    /**
+     * Establishes a connection to the Surreal database using the provided connection string.
+     *
+     * @param connect the connection string used to establish the connection
+     * @return the current instance of the {@code Surreal} class
+     */
     public Surreal connect(String connect) {
         connect(getPtr(), connect);
         return this;
     }
 
+    /**
+     * Attempts to sign in to the Surreal system using the provided credentials.
+     * The type of signin object determines the scope of the sign-in (Root, Namespace, or Database).
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealdb/security/authentication">authentication documentation</a>.
+     * <p>
+     *
+     * @param signin the credentials for signing in, which can be an instance of Root, Namespace, or Database
+     * @return a Token representing the session token after a successful sign-in
+     * @throws SurrealException if the signin type is unsupported
+     */
     public Token signin(Signin signin) {
         if (signin instanceof Database) {
             final Database db = (Database) signin;
@@ -121,34 +138,104 @@ public class Surreal extends Native implements AutoCloseable {
         throw new SurrealException("Unsupported sign in");
     }
 
+    /**
+     * Sets the namespace for the Surreal instance.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/use">use statement documentation</a>.
+     * <p>
+     *
+     * @param ns the namespace to use
+     * @return the current instance of the {@code Surreal} class
+     */
     public Surreal useNs(String ns) {
         useNs(getPtr(), ns);
         return this;
     }
 
+    /**
+     * Sets the database for the current instance of the Surreal class.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/use">use statement documentation</a>.
+     * <p>
+     *
+     * @param ns the database name to use
+     * @return the current instance of the {@code Surreal} class
+     */
     public Surreal useDb(String ns) {
         useDb(getPtr(), ns);
         return this;
     }
 
+    /**
+     * Executes a SurrealQL query on the database.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param sql the SurrealQL query to be executed
+     * @return a Response object containing the results of the query
+     */
     public Response query(String sql) {
         return new Response(query(getPtr(), sql));
     }
 
+    /**
+     * Executes a parameterized SurrealQL query on the database.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param sql    the SurrealQL query to be executed
+     * @param params a map containing parameter values to be bound to the SQL query
+     * @return a Response object containing the results of the query
+     */
     public Response queryBind(String sql, Map<String, ?> params) {
         return new Response(queryBind(getPtr(), sql, params));
     }
 
+    /**
+     * Creates a record in the database with the given `RecordID` as the key and the provided content as the value.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/create">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>     the type of the content
+     * @param thg     the RecordId associated with the new record
+     * @param content the content of the created record
+     * @return a new Value object initialized with the provided RecordId and content
+     */
     public <T> Value create(RecordId thg, T content) {
         final ValueMut valueMut = ValueBuilder.convert(content);
         final long valuePtr = createThingValue(getPtr(), thg.getPtr(), valueMut.getPtr());
         return new Value(valuePtr);
     }
 
+    /**
+     * Creates a record in the database with the given `RecordID` as the key and the provided content as the value.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/create">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param type    The class type of the object to create
+     * @param thg     The RecordId used with the new record
+     * @param content The content of the created record
+     * @return An instance of the specified type
+     */
     public <T> T create(Class<T> type, RecordId thg, T content) {
         return create(thg, content).get(type);
     }
 
+    /**
+     * Creates records in the database with the given table and the provided contents as the values.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/create">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>      the type of the contents
+     * @param target   the target for which the records are created
+     * @param contents the contents of the created records
+     * @return a list of Value objects created based on the target and contents
+     */
     @SafeVarargs
     public final <T> List<Value> create(String target, T... contents) {
         final long[] valueMutPtrs = contents2longs(contents);
@@ -156,6 +243,18 @@ public class Surreal extends Native implements AutoCloseable {
         return Arrays.stream(valuePtrs).mapToObj(Value::new).collect(Collectors.toList());
     }
 
+    /**
+     * Creates records in the database with the given table and the provided contents as the values.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/create">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>      the type of objects to be created
+     * @param type     the class of the type to be created
+     * @param target   the target string used in the creation process
+     * @param contents the contents to be used to create the objects
+     * @return a list of objects of the specified type
+     */
     @SafeVarargs
     public final <T> List<T> create(Class<T> type, String target, T... contents) {
         try (final Stream<Value> s = create(target, contents).stream()) {
@@ -163,6 +262,17 @@ public class Surreal extends Native implements AutoCloseable {
         }
     }
 
+    /**
+     * Insert records in the database with the given table and the provided contents as the values.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/insert">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>      the type of the contents
+     * @param target   the target for which the records are inserted
+     * @param contents the contents of the inserted records
+     * @return a list of Value objects inserted based on the target and contents
+     */
     @SafeVarargs
     public final <T> List<Value> insert(String target, T... contents) {
         final long[] valueMutPtrs = contents2longs(contents);
@@ -170,6 +280,18 @@ public class Surreal extends Native implements AutoCloseable {
         return Arrays.stream(valuePtrs).mapToObj(Value::new).collect(Collectors.toList());
     }
 
+    /**
+     * Insert records in the database with the given table and the provided contents as the values.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/insert">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>      the type of objects to be inserted
+     * @param type     the class of the type to be inserted
+     * @param target   the target string used in the insertion process
+     * @param contents the contents to be used to insert the objects
+     * @return a list of objects of the specified type
+     */
     @SafeVarargs
     public final <T> List<T> insert(Class<T> type, String target, T... contents) {
         try (final Stream<Value> s = insert(target, contents).stream()) {
@@ -177,16 +299,50 @@ public class Surreal extends Native implements AutoCloseable {
         }
     }
 
+    /**
+     * Inserts a relation to the specified table using the provided content.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/insert#insert-relation-tables">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param target  the table where the relation is to be inserted
+     * @param content the content to insert as the relation
+     * @param <T>     a type that extends InsertRelation
+     * @return a Value object representing the inserted relation
+     */
     public <T extends InsertRelation> Value insertRelation(String target, T content) {
         final ValueMut valueMut = ValueBuilder.convert(content);
         final long valuePtr = insertRelationTargetValue(getPtr(), target, valueMut.getPtr());
         return new Value(valuePtr);
     }
 
+    /**
+     * Inserts a relation of the specified type and table with the provided content.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/insert#insert-relation-tables">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>     the type of the relation that extends InsertRelation
+     * @param type    the class object of the type T
+     * @param target  the target identifier for the relation
+     * @param content the content to be inserted in the relation
+     * @return the inserted relation of type T
+     */
     public <T extends InsertRelation> T insertRelation(Class<T> type, String target, T content) {
         return insertRelation(target, content).get(type);
     }
 
+    /**
+     * Inserts relations into the specified table with the provided contents.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/insert#insert-relation-tables">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param target   the table into which the relations will be inserted
+     * @param contents the contents of the relations to be inserted
+     * @param <T>      the type of the insert relation
+     * @return a list of values representing the result of the insert operation
+     */
     @SafeVarargs
     public final <T extends InsertRelation> List<Value> insertRelations(String target, T... contents) {
         final long[] valueMutPtrs = contents2longs(contents);
@@ -194,6 +350,18 @@ public class Surreal extends Native implements AutoCloseable {
         return Arrays.stream(valuePtrs).mapToObj(Value::new).collect(Collectors.toList());
     }
 
+    /**
+     * Inserts multiple relations of a specified type into the target.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/insert#insert-relation-tables">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>      the type of InsertRelation
+     * @param type     the class type of the InsertRelation
+     * @param target   the table in which the relations are to be inserted
+     * @param contents the array of InsertRelation objects to be inserted
+     * @return a list of inserted relations of the specified type
+     */
     @SafeVarargs
     public final <T extends InsertRelation> List<T> insertRelations(Class<T> type, String target, T... contents) {
         try (final Stream<Value> s = insertRelations(target, contents).stream()) {
@@ -201,21 +369,70 @@ public class Surreal extends Native implements AutoCloseable {
         }
     }
 
+    /**
+     * Establishes a relation between two records identified by `from` and `to` within a specified table.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/relate">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param from  the record identifier from which the relation originates
+     * @param table the name of the table where the relation will be established
+     * @param to    the record identifier to which the relation points
+     * @return a new {@code Value} instance representing the relation
+     */
     public Value relate(RecordId from, String table, RecordId to) {
         final long valuePtr = relate(getPtr(), from.getPtr(), table, to.getPtr());
         return new Value(valuePtr);
     }
 
+    /**
+     * Establishes a relationship between two records in a specified table and returns the specified type of Relation.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/relate">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <T>  the type of Relation to return, which must extend the Relation class
+     * @param type the class object of type T, used to specify the type of the returned Relation
+     * @param from the RecordId representing the starting record of the relationship
+     **/
     public <T extends Relation> T relate(Class<T> type, RecordId from, String table, RecordId to) {
         return relate(from, table, to).get(type);
     }
 
+    /**
+     * Establishes a relationship between two records within a specified table,
+     * attaching the provided content to this relationship.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/relate">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param from    The record ID that the relationship starts from.
+     * @param table   The table in which the relationship is being created.
+     * @param to      The record ID that the relationship points to.
+     * @param content The content to attach to the relationship.
+     * @return A Value object representing the newly created relationship.
+     */
     public <T> Value relate(RecordId from, String table, RecordId to, T content) {
         final ValueMut valueMut = ValueBuilder.convert(content);
         final long valuePtr = relateContent(getPtr(), from.getPtr(), table, to.getPtr(), valueMut.getPtr());
         return new Value(valuePtr);
     }
 
+    /**
+     * Establishes a relation between two records and retrieves it based on the specified relation type.
+     * <p>
+     * For more details, check the <a href="https://surrealdb.com/docs/surrealql/statements/relate">SurrealQL documentation</a>.
+     * <p>
+     *
+     * @param <R>     the type of the relation
+     * @param <T>     the type of the content associated with the relation
+     * @param type    the class of the relation type
+     * @param from    the record identifier of the source record
+     * @param table   the name of the table where the relation is to be established
+     * @param to      the record identifier of the target record
+     * @param content the content to be associated with the relation
+     * @return the established relation of the specified type
+     */
     public <R extends Relation, T> R relate(Class<R> type, RecordId from, String table, RecordId to, T content) {
         return relate(from, table, to, content).get(type);
     }
